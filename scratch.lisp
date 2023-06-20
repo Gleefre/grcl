6f76f1b0cc4593f748a5f2c98b75b40806ed8529
Gleefre <varedif.a.s@gmail.com>
2023-06-20 22:26:28 +0200

    Setup packages

--- scratch.lisp
+++ scratch.lisp
@@ -2,3 +2,21 @@
 ;;;; Copyright 2023 Gleefre
 
 ;;;; This code is evaluated on the host implementation.
+
+(in-package #:cl-user)
+
+(defpackage #:grcl (:use))
+
+(defpackage #:grcl/host
+  (:use #:cl))
+
+(in-package #:grcl/host)
+
+;; @foo is used to read symbols in #:grcl package
+(defun |@-reader| (stream char)
+  (declare (ignore char))
+  (let ((symbol (read stream t nil t)))
+    (check-type symbol symbol)
+    (intern (symbol-name symbol) "GRCL")))
+
+(set-macro-character #\@ #'|@-reader|)
